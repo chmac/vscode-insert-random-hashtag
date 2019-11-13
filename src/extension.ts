255e70531358b78adf9a7eaaed6f449555c4f252
Callum Macdonald <git@callum-macdonald.com>
2019-11-13 21:11:56 +0100

    Insert random hash. Seems to work.

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ const CHARSET =
 
 // A function to generate a random hash value
 const generateRandomHash = (length: number = 6) => {
-  return Array.from({ length }).reduce(soFar => {
+  return Array.from({ length }).reduce((soFar: string): string => {
     const choice = Math.round(Math.random() * CHARSET.length);
     const newChar = CHARSET[choice];
     return `${soFar}${newChar}`;
@@ -17,24 +17,15 @@ const generateRandomHash = (length: number = 6) => {
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "helloworld-sample" is now active!'
-  );
-
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
+  let disposable = vscode.commands.registerTextEditorCommand(
     "extension.insertHashtag",
-    () => {
-      // The code you place here will be executed every time your command is executed
-
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        `Hello World! ${generateRandomHash()}`
-      );
+    (textEditor, edit) => {
+      textEditor.edit(editBuilder => {
+        editBuilder.insert(textEditor.selection.active, generateRandomHash());
+      });
     }
   );
 
